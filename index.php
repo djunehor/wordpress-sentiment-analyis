f9cd21f002a110ac67195c48245fa39d0359f2b4
makinde2013 <adeademi2@gmail.com>
2019-08-05 02:36:50 +0100

    add uninstall action

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: ZacWP Sentiment Analysis
-Description: Adds sentiment analysis for wp posts/pages and comments
+Description: Adds sentiment analysis for wp posts/pages and comments, and ability to filter based on sentiment.
 Version: 1.0
 Author: Zacchaeus Bolaji
 Author URI: https://github.com/makinde2013
@@ -9,6 +9,12 @@ Author URI: https://github.com/makinde2013
 // Exit if accessed directly
 defined( 'ABSPATH' ) or exit;
 
+
+/**Remove any block contents from the string
+ * @param $string
+ *
+ * @return string
+ */
 function zacwp_sa_clean_string($string) {
 	$string = str_split($string);
 	$paren_num = 0;
@@ -21,6 +27,11 @@ function zacwp_sa_clean_string($string) {
 	return trim($new_string);
 }
 
+/**Perform actual sentiment analysis on string
+ * @param $string
+ *
+ * @return mixed
+ */
 function zacwp_sa_sentiment_analysis($string) {
 
 	require_once (plugin_dir_path(__FILE__).'/inc/autoload.php');
@@ -34,80 +45,44 @@ function zacwp_sa_sentiment_analysis($string) {
 	return $result;
 }
 
-function zacwp_sa_preprocess_comment_handler( $commentdata ) {
-	$datum = zacwp_sa_sentiment_analysis($commentdata['comment_content']);
-	$string = zacwp_sa_set_score($datum['score']);
-
-	$meta['zacwp_sa_comment_score'] = $string;
-	$meta['zacwp_sa_comment_category'] = $datum['category'];
-
-	return array_merge($commentdata, $meta);
-}
-
-function zacwp_sa_filter_post_data( $data , $postarr ) {
-
-	$datum = zacwp_sa_sentiment_analysis($data['post_content']);
-	$string = zacwp_sa_set_score($datum['score']);
-
-	$meta['zacwp_sa_post_score'] = $string;
-	$meta['zacwp_sa_post_category'] = $datum['category'];
-
-	return array_merge($data, $meta);
-}
-
-function zacwp_sa_run_at_activation() {
+/**
+ *Remove plugin added post/comment meta
+ */
+function zacwp_sa_run_at_uninstall() {
 	global $wpdb;
-	zacwp_sa_add_not_exist($wpdb->prefix.'comments', 'zacwp_sa_comment_score', 'VARCHAR');
-	zacwp_sa_add_not_exist($wpdb->prefix.'posts', 'zacwp_sa_post_score', 'VARCHAR');
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."postmeta WHERE `meta_key`='zacwp_sa_post_category' OR `meta_key`='zacwp_sa_post_score'");
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."commentmeta WHERE `meta_key`='zacwp_sa_comment_category' OR `meta_key`='zacwp_sa_comment_score'");
 }
 
-function zacwp_sa_run_at_deactivation() {
-	global $wpdb;
-	zacwp_sa_drop_if_exist($wpdb->prefix.'comments', 'zacwp_sa_comment_score');
-	zacwp_sa_drop_if_exist($wpdb->prefix.'posts', 'zacwp_sa_post_score');
-}
-
-function zacwp_sa_add_not_exist($table, $field, $type)
-{
-	global $wpdb;
-	$results = $wpdb->get_results("SHOW columns FROM `".$table."` where field='".$field."'");
-	if (!count($results))
-	{
-		$type = strtoupper($type);
-		$sql = "ALTER TABLE  `".$table."` ADD `".$field."` ";
-		$sql .= $type == 'VARCHAR' ? $type."(255)" : $type == 'INT' ? $type."(11)" : $type;
-		$sql .= " NULL";
-		$wpdb->query($sql);
-	}
-}
-
-function zacwp_sa_drop_if_exist ($table, $field)
-{
-	global $wpdb;
-	$results = $wpdb->get_results("SHOW columns FROM `".$table."` where field='".$field."'");
-	if (count($results))
-	{
-		$sql = "ALTER TABLE `$table` DROP COLUMN `$field`;";
-		$wpdb->query($sql);
-	}
-}
-
-//register_activation_hook( __FILE__, 'zacwp_sa_run_at_activation' );
-//register_deactivation_hook( __FILE__, 'zacwp_sa_run_at_deactivation' );
-
-function zacwp_sa_comment_columns( $columns )
+/**Add sentiment score column to the admin comments table
+ * @param $columns
+ *
+ * @return mixed
+ */
+function zacwp_sa_comments_add_sentiment_score_column( $columns )
 {
 	$columns['zacwp_sa_comment_score'] = __( 'Sentiment Score' );
 	return $columns;
 }
 
-function zacwp_sa_post_columns( $columns )
+/**Add sentiment score column to the admin posts/pages table
+ * @param $columns
+ *
+ * @return mixed
+ */
+function zacwp_sa_posts_add_sentiment_score_column( $columns )
 {
 	$columns['zacwp_sa_post_score'] = __( 'Sentiment Score' );
 	return $columns;
 }
 
-function zacwp_sa_comment_column( $column, $comment_ID )
+/**Runs for each comment row
+ * Calculate and save sentiment analysis for those without
+ * Display sentiment analysis for current row
+ * @param $column
+ * @param $comment_ID
+ */
+function zacwp_sa_comments_add_sentiment_analysis_row( $column, $comment_ID )
 {
 	if ( 'zacwp_sa_comment_score' == $column ) {
 		$score = get_comment_meta( $comment_ID, 'zacwp_sa_comment_score', true );
@@ -128,7 +103,14 @@ function zacwp_sa_comment_column( $column, $comment_ID )
 
 }
 
-function zacwp_sa_post_column( $column, $post_ID )
+/**Runs for each post column
+ * Calculate and save sentiment analysis for those without
+ * Display sentiment analysis for current row
+ *
+ * @param $column
+ * @param $post_ID
+ */
+function zacwp_sa_posts_add_sentiment_analysis_row( $column, $post_ID )
 {
 	if ( 'zacwp_sa_post_score' == $column) {
 		$score = get_post_meta( $post_ID, 'zacwp_sa_post_score', true );
@@ -146,6 +128,11 @@ function zacwp_sa_post_column( $column, $post_ID )
 	}
 }
 
+/**Return formatted sentiment category with score percent
+ * @param $array
+ *
+ * @return string
+ */
 function zacwp_sa_set_score($array) {
 	$max = max($array);
 
@@ -166,7 +153,10 @@ function zacwp_sa_set_score($array) {
 	}
 }
 
-function zacwp_update_post_sentiment($post_ID) {
+/**Update specified post sentiment score and category
+ * @param $post_ID
+ */
+function zacwp_sa_update_post_sentiment($post_ID) {
 	$post = get_post( $post_ID, ARRAY_A);
 	$meta = zacwp_sa_sentiment_analysis($post['post_content']);
 	$score = zacwp_sa_set_score($meta['score']);
@@ -174,19 +164,22 @@ function zacwp_update_post_sentiment($post_ID) {
 	update_post_meta($post_ID, 'zacwp_sa_post_category', $meta['category']);
 }
 
-function zacwp_update_comment_sentiment($post_ID) {
-	$post = get_post( $post_ID, ARRAY_A);
+/**Update specified comment sentiment score and category
+ *
+ * @param $comment_ID
+ */
+function zacwp_sa_update_comment_sentiment($comment_ID) {
+	$post = get_post( $comment_ID, ARRAY_A);
 	$meta = zacwp_sa_sentiment_analysis($post['comment_content']);
 	$score = zacwp_sa_set_score($meta['score']);
-	update_comment_meta($post_ID, 'zacwp_sa_comment_score', $score);
-	update_comment_meta($post_ID, 'zacwp_sa_comment_category', $meta['category']);
+	update_comment_meta($comment_ID, 'zacwp_sa_comment_score', $score);
+	update_comment_meta($comment_ID, 'zacwp_sa_comment_category', $meta['category']);
 }
 
-add_filter( 'parse_query', 'zacwp_sa_admin_posts_filter' );
-add_filter('the_comments', 'zacwp_sa_filter_comments');
-add_action( 'restrict_manage_posts', 'zacwp_sa_admin_posts_filter_restrict_manage_posts' );
-add_action( 'restrict_manage_comments', 'zacwp_sa_admin_comments_filter_restrict_manage_posts' );
 
+/**Add sentiment analysis filter for post
+ * @param $query
+ */
 function zacwp_sa_admin_posts_filter( $query )
 {
 	global $pagenow;
@@ -197,11 +190,17 @@ function zacwp_sa_admin_posts_filter( $query )
 	}
 }
 
+/**Filter comments based on specified sentiment
+ *
+ * @param $comments
+ *
+ * @return mixed
+ */
 function zacwp_sa_filter_comments($comments){
 	global $pagenow;
 	if($pagenow == 'edit-comments.php'
-		&& isset($_GET['sentiment_type'])
-		&& !empty($_GET['sentiment_type'])
+	   && isset($_GET['sentiment_type'])
+	   && !empty($_GET['sentiment_type'])
 	){
 		foreach($comments as $i => $comment){
 
@@ -211,7 +210,11 @@ function zacwp_sa_filter_comments($comments){
 	return $comments;
 }
 
-function zacwp_sa_admin_posts_filter_restrict_manage_posts()
+/**Add sentiment filter form to the posts page
+ *
+ * @return mixed
+ */
+function zacwp_sa_posts_add_sentiment_score_filter_form()
 {
 	$values = [
 		'neu' => 'Neutral',
@@ -219,8 +222,8 @@ function zacwp_sa_admin_posts_filter_restrict_manage_posts()
 		'neg' => 'Negative'
 	];
 	?>
-	<select name="post_sentiment_type">
-		<option value=""><?php _e('Filter By Sentiment', 'commmm'); ?></option>
+    <select name="post_sentiment_type">
+        <option value=""><?php _e('Filter By Sentiment', 'commmm'); ?></option>
 		<?php
 		$current = isset($_GET['post_sentiment_type'])? $_GET['post_sentiment_type']:'';
 		foreach ($values as $key => $value) {
@@ -234,11 +237,15 @@ function zacwp_sa_admin_posts_filter_restrict_manage_posts()
 
 		}
 		?>
-	</select>
+    </select>
 	<?php
 }
 
-function zacwp_sa_admin_comments_filter_restrict_manage_posts()
+/**Add sentiment filter form to the comments page
+ *
+ * @return mixed
+ */
+function zacwp_sa_comments_add_sentiment_score_filter_form()
 {
 
 
@@ -248,8 +255,8 @@ function zacwp_sa_admin_comments_filter_restrict_manage_posts()
 		'neg' => 'Negative'
 	];
 	?>
-	<select name="sentiment_type">
-		<option value=""><?php _e('Filter By Sentiment', 'commmm'); ?></option>
+    <select name="sentiment_type">
+        <option value=""><?php _e('Filter By Sentiment', 'commmm'); ?></option>
 		<?php
 		$current = isset($_GET['sentiment_type'])? $_GET['sentiment_type']:'';
 		foreach ($values as $key => $value) {
@@ -263,23 +270,79 @@ function zacwp_sa_admin_comments_filter_restrict_manage_posts()
 
 		}
 		?>
-	</select>
+    </select>
 	<?php
 }
 
-add_filter( 'manage_edit-comments_columns', 'zacwp_sa_comment_columns' );
-add_filter( 'manage_posts_columns', 'zacwp_sa_post_columns' );
-add_filter( 'manage_pages_columns', 'zacwp_sa_post_columns' );
+/**Add sentiment analysis meta to post if not exist
+ *
+ * @param $post_id
+ *
+ * @return mixed
+ */
+function zacwp_sa_analyze_new_edited_post( $post_id ) {
 
-add_filter( 'manage_comments_custom_column', 'zacwp_sa_comment_column', 10, 2 );
-add_filter( 'manage_posts_custom_column', 'zacwp_sa_post_column', 11, 2 );
-add_filter( 'manage_pages_custom_column', 'zacwp_sa_post_column', 12, 2 );
+	// If this is a revision, don't send the email.
+	if ( wp_is_post_revision( $post_id ) )
+		return;
 
-add_filter( 'wp_insert_post_data' , 'zacwp_sa_filter_post_data' , '99', 2 );
-add_filter( 'wp_insert_page_data' , 'zacwp_sa_filter_post_data' , '100', 2 );
-add_filter( 'preprocess_comment' , 'zacwp_sa_preprocess_comment_handler' );
-do_action( 'edit_comment', 'zacwp_update_comment_sentiment', 10, 2 );
-do_action( 'edit_post', 'zacwp_update_post_sentiment', 10, 2 );
+	$score = get_post_meta( $post_id, 'zacwp_sa_post_score', true );
+	if ( empty($score)) {
+		$post = get_post( $post_id, ARRAY_A);
+		$meta = zacwp_sa_sentiment_analysis($post['post_content']);
+		$score = zacwp_sa_set_score($meta['score']);
+		update_post_meta($post_id, 'zacwp_sa_post_score', $score);
+		update_post_meta($post_id, 'zacwp_sa_post_category', $meta['category']);
+
+	}
+
+}
+
+/**Add sentiment analysis meta to comment if not exist
+ *
+ * @param $comment_id
+ * @param $comment_approved
+ *
+ * @return mixed
+ */
+function zacwp_sa_analyze_new_edited_comment( $comment_id, $comment_approved ) {
+
+	$score = get_post_meta( $comment_id, 'zacwp_sa_comment_score', true );
+	if ( empty($score)) {
+		$post = get_comment( $comment_id, ARRAY_A);
+		$meta = zacwp_sa_sentiment_analysis($post['comment_content']);
+		$score = zacwp_sa_set_score($meta['score']);
+		update_comment_meta($comment_id, 'zacwp_sa_comment_score', $score);
+		update_comment_meta($comment_id, 'zacwp_sa_comment_category', $meta['category']);
+
+	}
+
+}
+
+//run when plugin is uninstalled
+register_uninstall_hook( __FILE__, 'zacwp_sa_run_at_uninstall' );
+
+//add sentiment filters
+add_filter( 'parse_query', 'zacwp_sa_admin_posts_filter' );
+add_filter('the_comments', 'zacwp_sa_filter_comments');
+
+//add filter form
+add_action( 'restrict_manage_posts', 'zacwp_sa_posts_add_sentiment_score_filter_form' );
+add_action( 'restrict_manage_comments', 'zacwp_sa_comments_add_sentiment_score_filter_form' );
+
+//perform when comment/post is created/updated
+add_action( 'wp_insert_post', 'zacwp_sa_analyze_new_edited_post', 10, 3 );
+add_action( 'comment_post', 'zacwp_sa_analyze_new_edited_comment', 10, 2 );
+
+//add sentiment score column to admin tables
+add_filter( 'manage_edit-comments_columns', 'zacwp_sa_comments_add_sentiment_score_column' );
+add_filter( 'manage_posts_columns', 'zacwp_sa_posts_add_sentiment_score_column' );
+add_filter( 'manage_pages_columns', 'zacwp_sa_posts_add_sentiment_score_column' );
+
+//deduce and display sentiment score for each post/comment
+add_filter( 'manage_comments_custom_column', 'zacwp_sa_comments_add_sentiment_analysis_row', 10, 2 );
+add_filter( 'manage_posts_custom_column', 'zacwp_sa_posts_add_sentiment_analysis_row', 11, 2 );
+add_filter( 'manage_pages_custom_column', 'zacwp_sa_posts_add_sentiment_analysis_row', 12, 2 );
 
 
 ?>
